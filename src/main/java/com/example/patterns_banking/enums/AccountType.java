f1871cd17d91d157e326fb6b50858f979de42ede
Jorge Luis Bermudez Buendia <george-1002@hotmail.com>
2025-04-22 23:25:26 -0500

    add class Account.java, AccountType.java, AccountDTO.java, IAccountRepository.java, IAccountRepository.java, AccountService.java, AccountController.java

---
+++ src/main/java/com/example/patterns_banking/enums/AccountType.java
@@ -0,0 +1,10 @@
+package com.example.patterns_banking.enums;
+
+public enum AccountType {
+    AHORRO,
+    CORRIENTE,
+    EMPRESARIAL,
+    CUENTA_INFANTIL,
+    MONEDA_EXTRANJERA,
+    CUENTA_A_PLAZO
+}
